a25feb583f9b8064413ab1f214ed2036eddb0e4e
Zoltán T <tzg6sa@gmail.com>
2020-09-05 17:50:07 +0200

    tl270u modifications

--- pn_plot_tl270u.py
+++ pn_plot_tl270u.py
@@ -8,32 +8,21 @@ import os.path
 import re, os, logging
 
 my_circuit =  "cmos_60G_tl270u"
-# my_circuit =  "cmos_60G_sf"
-# my_circuit =  "cmos_60G"
-# my_circuit =  "nmos_60G_sf"
-w_dac = False
-w_short = True
 do_show_all =False
 # annotate_vdd = True
 annotate_vdd = False
+plot_sim = False
 
-# style_sim  = {"linestyle":"--", "color":"grey", "marker":"None", "label":"sim"} # for plot
-style_sim  = {"linestyle":"--", "color":"grey", "marker":"None", "label":"simulation"} # for plot
+fig_style = {"figsize":(3.3914487339144874, 2.0960305886619515*0.8)}
+# style_sim  = {"linestyle":"--", "color":"grey", "marker":"None", "label":"simulation"} # for plot
 
-pn_dir = "/mnt/home/documents/Measurements/MPW2215_VCO/Phase_noise/"
-pn_sim_dir = "/mnt/home/documents/Design/pictures/vco_mpw2215/"
-pn_dir = "/home/zoltan/ccn/Measurements/MPW2215_VCO/Phase_noise/"
-pn_sim_dir = "/home/zoltan/ccn/Design/pictures/vco_mpw2215/"
-if my_circuit == "cmos_60G_sf":
-    # re_pn = re.compile("(?P<circuit>.*)_set_values_vtune(?P<vtune>[0-9]+(.[0-9]+)?)V_vdd(?P<vdd>[0-9]p[0-9])V_ib(?P<ib>[0-9])mA_ibuf(?P<ibuf>[0-9]+)uA(?P<note>.*)") # older meas
-    re_pn = re.compile("(?P<circuit>.*)_vtune(?P<vtune>[0-9]+(.[0-9]+)?)V_vdd(?P<vdd>[0-9]p[0-9])V_ib(?P<ib>[0-9])mA_ibuf(?P<ibuf>[0-9]+)uA(?P<note>.*)")
-elif my_circuit == "nmos_60G_sf":    
-    re_pn = re.compile("(?P<circuit>.*)_vtune(?P<vtune>[0-9]+(.[0-9]+)?)V_vdd(?P<vdd>[0-9]p[0-9])V_ib(?P<ib>[0-9]+)mA?_ibuf(?P<ibuf>[0-9]+)uA(?P<note>.*)")
-else:    
-    re_pn = re.compile("(?P<circuit>.*)_vtune(?P<vtune>[0-9]+(.[0-9]+)?)V_vdd(?P<vdd>[0-9]p[0-9])V_ib(?P<ib>[0-9]+)mA_(?P<note>.*)")
+pn_dir      = "/home/zoltan/ccn/Measurements/MPW2215_VCO/Phase_noise/"
+pn_sim_dir  = "/home/zoltan/ccn/Measurements/MPW2215_VCO/sim/"
+
+re_pn = re.compile("(?P<circuit>.*)_vtune(?P<vtune>[0-9]+(.[0-9]+)?)V_vdd(?P<vdd>[0-9]p[0-9])V_ib(?P<ib>[0-9]+)mA_(?P<note>.*)")
 
 
-logger = logging.getLogger("pn_plot_all")
+logger = logging.getLogger("pn_plot")
 logger.setLevel(logging.DEBUG)
 # create console handler and set level to debug
 ch = logging.StreamHandler()
@@ -46,22 +35,15 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 # vcsv_cols is used to select the columns corresponding to the different supply voltages. It was easier than writing a parser, because the name of the vcsv columns might be changed by hand --> the universal formula has to handle different ways of defining the VDD.
-d_sim = {'nmos_60G_sf':{ 'f_sim'     : 'nmos_sf_pn_all_supply_10mA_cc_only.vcsv',
-                         'vcsv_cols' : {'0p8':1,'1p0':3,'1p2':5}  
-                         }, # nmos_60G_sf
-        'cmos_60G_sf':{ 'f_sim'     : 'cmos_sf_pn_3mA_275uA_allsupply.vcsv',
-                         'vcsv_cols' : {'0p8':1,'1p0':3,'1p2':5,'1p4':7}
-                         }, # cmos_60G_sf
-        'cmos_tl270u_60G':{ 'f_sim'     : 'cmos_tl270u_pn_all_supply_4mA.vcsv',
-                         'vcsv_cols' : {'1p0':1,'1p2':3,'1p4':5,'1p6':7}
-                         }, # cmos_tl270u_60G
-        'cmos_60G_tl270u':{ 'f_sim'     : 'cmos_tl270u_pn_all_supply_4mA.vcsv',
-                         'vcsv_cols' : {'1p0':1,'1p2':3,'1p4':5,'1p6':7}
-                         }, # cmos_tl270u_60G
-        'cmos_60G':      { 'f_sim'     : 'cmos_60G_pn_merged.csv',
-                         'vcsv_cols' : {'1p0':1,'1p2':3,'1p4':5,'1p6':7}
-                         }, # cmos_60G
+d_sim = { 'f_sim'     : 'cmos_tl270u_pn_all_supply_20200419.vcsv',
+                         'vcsv_cols' : {'1p0':7,'1p2':5,'1p4':3,'1p6':1}
                          }
+d_color = { '1p0': '#0037ff', # blue
+            '1p6': '#ff7300'  # its complementary color: orange
+          }
+d_txt_offset = { '1p0': (-0,5),
+                 '1p6': (-0.8,-8)
+                }
 
 latex=True
 if latex:
@@ -73,12 +55,21 @@ if latex:
         # Use 10pt font in plots, to match 10pt font in document
         # "axes.labelsize": 10,
         "axes.labelsize": 8,
+        "axes.grid" :True,
         "font.size": 10,
         # Make the legend/label fonts a little smaller
         "legend.fontsize": 8,
         "legend.title_fontsize": 8,
         "xtick.labelsize": 8,
         "ytick.labelsize": 8,
+        "xtick.direction":"in",
+        "ytick.direction":"in",
+        "xtick.major.size":1.5,
+        "ytick.major.size":1.5,
+        "savefig.pad_inches":0,
+        "savefig.bbox":"tight",
+        "savefig.dpi":600,
+        'lines.markersize':4,
         "pgf.texsystem": "lualatex",
         "pgf.preamble": [
             "\\usepackage{siunitx}",         
@@ -88,12 +79,12 @@ if latex:
             r"\DeclareSIUnit{\dBc}{dBc}",
             r"\DeclareSIUnit[per-mode=symbol]{\dBcHz}{\dBc\per\Hz}",
             r"\newcommand{\da}{\textsuperscript{$\dagger$}}"
-            r"\setmathfont{xits-math.otf}",
-            r"\setmainfont{DejaVu Serif}", # serif font via preamble
+            # r"\setmathfont{xits-math.otf}",
+            # r"\setmainfont{DejaVu Serif}", # serif font via preamble
             ]
     }
     matplotlib.rcParams.update(pgf_with_custom_preamble)
-    matplotlib.rcParams['lines.markersize'] = 4
+    matplotlib.rcParams['axes.unicode_minus'] = False
 
 
 # building list of available measurement results
@@ -107,20 +98,20 @@ for i in os.listdir(pn_dir):
             if vdd not in l_vdd:
                 l_vdd.append(vdd)
 logger.info('Available measurement results: ' + ", ".join(l_vdd))
-l_vdd_sim = [ i.replace('.','p') for i in d_sim.get(my_circuit).get('vcsv_cols').keys()]
+l_vdd_sim = [ i.replace('.','p') for i in d_sim.get('vcsv_cols').keys()]
 
-for my_vdd in set().union(l_vdd, l_vdd_sim):    
-    fig, ax = plt.subplots(figsize=(3.3914487339144874, 2.0960305886619515*0.85))
+fig, ax = plt.subplots(**fig_style)
+for my_vdd in set().union(l_vdd, l_vdd_sim).intersection(d_color.keys()):    
     # plotting the sim
     logger.info("VDD=" + my_vdd + " is being processed")
-    if my_vdd in l_vdd_sim: # and False:
+    if my_vdd in l_vdd_sim and plot_sim: # and False:
         d_sim_vdd = d_sim.get(my_circuit)
         logger.debug(f"d_sim_vdd: f{d_sim_vdd}")
         my_cols = [0]
         my_cols.append( d_sim_vdd.get('vcsv_cols').get(my_vdd) ) 
         logger.debug(f"VDD={my_vdd};\tUsed csv columns: {my_cols}")
         df_sim = pd.read_csv(pn_sim_dir + d_sim_vdd.get('f_sim'), header=None, skiprows=6, dtype=np.float64, usecols=my_cols, names=['f','pn'])
-        logger.info(f"Adding simulation: {d_sim_vdd.get('f_sim')}")
+        logger.info(f"Adding simulation: {pn_sim_dir + d_sim_vdd.get('f_sim')}")
         df_sim.f *= 1e-6
         ax.plot(df_sim.f, df_sim.pn, **style_sim)
 
@@ -128,8 +119,10 @@ for my_vdd in set().union(l_vdd, l_vdd_sim):
         # ax.text(x-7.5,pn10-10,"$\SI{%.0f}{\dBcHz}$" % pn10, fontsize=7, color=style_sim.get("color"))
         # ax.scatter(x,pn10, marker='o',color=style_sim.get("color"),s=20)
 
-        x,pn1 = df_sim[df_sim.f==1].values[0]
-        ax.text(x-0.7,pn1-7,"$\SI{%.0f}{\dBcHz}" % pn1, fontsize=7, color=style_sim.get("color"))
+        func = interp1d(df_sim.f, df_sim.pn, kind='cubic')
+        x,pn1 = 1,func(1)
+        # x,pn1 = df_sim[df_sim.f==1].values[0]
+        ax.text(x-0.73,pn1-9,"$\SI{%.1f}{\dBcHz}" % pn1, fontsize=7, color=style_sim.get("color"))
         ax.scatter(x,pn1, marker='o',color=style_sim.get("color"),s=20)
 
         if annotate_vdd:
@@ -161,10 +154,10 @@ for my_vdd in set().union(l_vdd, l_vdd_sim):
                         df['f'] *= 1e-6
                         l_df.append(df)
                         if do_show_all:
-                            df.plot(x='f', y='pn', logx=True, grid=True, ax=ax, label=note)
+                            df.plot(x='f', y='pn', logx=True, grid=True, ax=ax, label=note, color=d_color.get(my_vdd))
                         else:
                             # df.plot(x='f', y='pn', logx=True, grid=True, ax=ax, kind='scatter', alpha=0.005, color="k")
-                            ax.plot(df.f, df.pn, alpha=0.05, color="k")
+                            ax.plot(df.f, df.pn, alpha=0.05, color=d_color.get(my_vdd))
                         x,pn10 = df[df.f==10].values[0]
                         x,pn1 = df[df.f==1].values[0]
                         logger.debug(f"{os.path.basename(i)}\t%.1f %.1f" % (pn1, pn10))
@@ -178,38 +171,36 @@ for my_vdd in set().union(l_vdd, l_vdd_sim):
         if not do_show_all:
             df_avg = pd.concat(l_df).groupby(level=0).mean().sort_values(by=['f'])
             df_avg.to_csv(file_basename + ".csv", index=False )
-
-            ax.plot(df_avg.f, df_avg.pn, color="k") # , label="meas" )
+            ax.plot(df_avg.f, df_avg.pn, color=d_color.get(my_vdd), label=r'\SI{' +my_vdd.replace("p",".") + r'}{\volt}' )
             
             func = interp1d(df_avg.f, df_avg.pn, kind='cubic')
             x,pn1 = 1,func(1)
-            ax.text(x-0.1, pn1+5,"$\SI{%.0f}{\dBcHz}" % pn1, fontsize=7)
-            ax.scatter(x,pn1, marker='o',color='k',s=20)
+            off = d_txt_offset.get(my_vdd)
+            ax.text(x+off[0], pn1+off[1],"$\SI{%.1f}{\dBcHz}" % pn1, fontsize=7, color=d_color.get(my_vdd))
+            ax.scatter(x,pn1, marker='o',color=d_color.get(my_vdd),s=20)
 
 
-    ax.set_xlabel(r"$\Delta f \left[\si{\MHz}\right]")
-    ax.set_ylabel(r"$\mathcal{L}(f_0) \left[\si{\dBcHz} \right]$")
-    ax.set_xscale("log")
-    ax.grid(which='major', alpha=0.5)
-    ax.grid(which='minor', alpha=0.2)
-    ax.legend(frameon=False)
-    ax.xaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter('{x:n}'))
-    ax.set_xlim(0.1,100)
-    ax.set_ylim(-120,-50)
-    if my_circuit == "nmos_60G_sf":
-        # ax.set_xlim(0.3,100)
-        pass
-    # else:
-    if do_show_all:
-        ax.legend()
-        suffix = "_all"
-    else:
-        suffix = ""
+ax.set_xlabel(r"$\Delta f \left[\si{\MHz}\right]",labelpad=0)
+ax.set_ylabel(r"$\mathcal{L}(f_0) \left[\si{\dBcHz} \right]$",labelpad=1)
+ax.set_xscale("log")
+ax.grid(which='major', alpha=0.5)
+ax.grid(which='minor', alpha=0.2)
+ax.legend(frameon=False) #, title=r"V\textsubscript{DD}" )
+ax.xaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter('{x:n}'))
+ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(10))
+ax.set_xlim(0.1,100)
+ax.set_ylim(-130,-50)
+if do_show_all:
+    ax.legend()
+    suffix = "_all"
+else:
+    suffix = ""
 
-    for ext in [".pgf",".png"]:
-    # for ext in [".png"]:
-        file_basename = pn_dir + "%s_%sV_pn" % (my_circuit, my_vdd)
-        fig.savefig(file_basename + suffix + ext, bbox_inches="tight", dpi=600)
+for ext in [".pgf",".png"]:
+# for ext in [".png"]:
+    file_basename = pn_dir + "%s_pn" % (my_circuit)
+    fig.savefig(file_basename + suffix + ext)
 
-plt.show()
+if not latex:
+    plt.show()
 
---
+++ vco_cmos_tl270u.py
@@ -0,0 +1,367 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+from scipy import optimize
+from functools import partial
+import skrf as rf
+import re
+
+do_pn       = False
+do_title    = True
+
+meas_dir = "/mnt/home/documents/Measurements/MPW2215_VCO/"
+meas_dir = "/home/zoltan/ccn/Measurements/MPW2215_VCO/"
+sim_dir = "/home/zoltan/ccn/Measurements/MPW2215_VCO/sim/"
+
+style_sim  = {"linestyle":"--", "color":"grey", "marker":"None"} #, "label":"sim"} # for plot
+style_meas = {"linestyle":":", "color":"black", "marker":"o","s":40} # , "label":"meas"}  # for scatter
+style_meas1 = {"linestyle":"-", "edgecolor":None,"facecolor":"blue",  "s":20, "marker":"o"} #, "label":"meas", "marker":"o"}
+style_meas2 = {"linestyle":"-", "edgecolor":None,"facecolor":"green", "s":20, "marker":"v"} #, "label":"meas", "marker":"o"}
+style_meas3 = {"linestyle":"-", "edgecolor":None,"facecolor":"orange", "s":20, "marker":"s"} #, "label":"meas", "marker":"o"}
+style_meas4 = {"linestyle":"-", "edgecolor":None,"facecolor":"purple", "s":20, "marker":"^"} #, "label":"meas", "marker":"o"}
+legend_style = {"frameon":False, "fontsize":7, "handletextpad":0.4, "borderaxespad":0, "ncol":3, "loc":'lower center', "mode":"expand", "handlelength":2, "bbox_to_anchor":(0.16,1.0,0.84,0.04)}
+legend_style_ncol2 = {"frameon":False, "fontsize":7, "handletextpad":0.4, "borderaxespad":0, "ncol":2, "loc":'lower center', "mode":"expand", "handlelength":2, "bbox_to_anchor":(0.2,1.0,0.75,0.04)} # for no simulation
+legend_style2 = {"frameon":False, "fontsize":7, "handletextpad":0.4, "borderaxespad":0, "ncol":1, "loc":'lower center', "mode":"expand", "handlelength":1, "labelspacing":0.3, "title_fontsize":8}
+legend_style2_ncol2 = {"frameon":False, "fontsize":7, "handletextpad":0.4, "borderaxespad":0, "ncol":2, "loc":'lower center', "mode":"expand", "handlelength":1, "title_fontsize":8, "labelspacing":0.3}
+legend_style2_ncol2_ul = {"frameon":False, "fontsize":7, "handletextpad":0.4, "borderaxespad":0, "ncol":2, "loc":'upper left', "mode":"expand", "handlelength":0.6}
+vdd_ib_annot = {"s":r"V\textsubscript{DD} I\textsubscript{b}:","xy":(0,1.12),"xycoords":"axes fraction","fontsize":8}
+l_style_meas = [style_meas1, style_meas2,style_meas3,style_meas4]
+
+plot_style = {"figsize":(3.3914487339144874, 2.0960305886619515*0.7)}
+
+xlabel = {"xlabel":r"Frequency $\left[\si{\GHz}\right]$", "labelpad":0}
+opt_vcsv = {'header':None, 'skiprows':6, 'dtype':np.float64, 'usecols':[0,1,3,5],'names':['vtune','freq','pout','pdc']} 
+
+def eff(pout, vdc, idc, in_dBm=False):
+    """ Calculating the DC-to-RF efficiency.
+        
+        Paramters
+        ---------
+        pout:       float
+                    output power
+        vdc:        float
+                    dc supply voltage
+        idc:        float
+                    dc suply current
+        in_dBm:     boolean, optional
+                    is pout value in dBm?
+        
+        Returns
+        -------
+        float
+                   DC-to-RF efficiency in percentage.
+    """
+    if in_dBm:
+        ret = (10**(pout/10))*1e-3/vdc/idc*100
+    else:
+        ret = pout/vdc/idc*100
+    return ret
+
+latex=True
+if latex:
+    matplotlib.use('pgf')
+    pgf_with_custom_preamble = {
+        "font.family": "serif", # use serif/main font for text elements
+        "text.usetex": True,    # use inline math for ticks
+        "pgf.rcfonts": False,   # don't setup fonts from rc parameters
+        # "figure.dpi" : 300,
+        # "figure.autolayout" : True,
+        # Use 10pt font in plots, to match 10pt font in document
+        # "axes.labelsize": 10,
+        "axes.labelsize": 8,
+        "axes.grid" :True,
+        "font.size": 10,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": 8,
+        "legend.title_fontsize": 8,
+        "xtick.labelsize": 8,
+        "ytick.labelsize": 8,
+        "xtick.direction":"in",
+        "ytick.direction":"in",
+        "xtick.major.size":1.5,
+        "ytick.major.size":1.5,
+        # "xtick.major.pad":2,
+        # "ytick.major.pad":2,
+        "grid.alpha": 0.5,
+        'lines.markersize':4,
+        "savefig.pad_inches":0,
+        "savefig.bbox":"tight",
+        "savefig.dpi":300,
+        "pgf.preamble": [
+            "\\usepackage{siunitx}",         # load additional packages
+            "\\usepackage{amsmath}",         # load additional packages
+            "\\usepackage{metalogo}",
+            "\\usepackage{unicode-math}",  # unicode math setup
+            r"\newcommand{\da}{\textsuperscript{$\dagger$}}"
+            r"\DeclareSIUnit{\Bm}{Bm}",
+            r"\DeclareSIUnit{\dBm}{\deci\Bm}",
+            r"\sisetup{detect-weight=true, detect-family=true, per-mode=fraction, fraction-function=\tfrac,range-phrase=--, range-units=single}",
+            # r"\setmathfont{xits-math.otf}",
+            # r"\setmainfont{DejaVu Serif}", # serif font via preamble
+            ]
+    }
+    matplotlib.rcParams.update(pgf_with_custom_preamble)
+
+#######################################
+#   Cable loss interpolation 
+#######################################
+f_loss = "Probe_842922_cable_05439601_dcblock_avg.s2p"
+f_loss = "2tier_cable_SN06149358_D7032_CSR8_Thru_DCblock.s2p"
+nw_loss = rf.Network(meas_dir + f_loss)
+
+def loss_func(f, dc,sq,lin):
+    return dc + sq*np.sqrt(f) + lin*f
+
+x = nw_loss.f
+y = nw_loss.s_db[:,1,0]
+par, cov = optimize.curve_fit(loss_func, x, y)
+cable_loss = partial(loss_func, dc=par[0], sq=par[1], lin=par[2])
+
+#################################
+# Measurement results from Excel
+#################################
+meas_xlsx = meas_dir + "vco_cmos_60G_TL270u.xlsx"
+df_meas = pd.read_excel(meas_xlsx, usecols="B:G",sheet_name="20200218", header=0).rename(columns={"IDD[m]":"idd", "freq [GHz]":"freq", "Pout [dBm]":"pout", "Vtune":"vtune", "VDD":"vdd", "Iosc[mA]":"ib"})
+df_meas = pd.read_excel(meas_xlsx, usecols="B:G",sheet_name="20200522", header=0).rename(columns={"IDD[m]":"idd", "freq [GHz]":"freq", "Pout [dBm]":"pout", "Vtune":"vtune", "VDD":"vdd", "Iosc[mA]":"ib"})
+# l_my_vdd = [1, 1.6]
+
+l_cond = [
+        {"vdd":1,"ib":4},
+        {"vdd":1.2,"ib":4},
+        {"vdd":1.4,"ib":4},
+        # {"vdd":1.4,"ib":6} # higher output power, but slightly lower efficiency
+        {"vdd":1.6,"ib":6}
+        ]
+# p_out supposed to be the DIFFERENTIAL output power available at the PADS
+# The single ended output was measured
+df_meas.pout += 3 - cable_loss(df_meas.freq*1e9) # it should be moved to the common part
+df_meas["eff"] = eff(df_meas.pout, df_meas.vdd, df_meas.idd*1e-3, in_dBm=True) 
+
+suffix = "cmos_tl270u"
+outfile = suffix + ".png"
+
+df_sim1 = pd.read_csv(sim_dir + 'cmos_tl270u_vdd1p0V_ib4mA_20200527_tt_90C.vcsv', **opt_vcsv)
+df_sim1['vdd'] = 1
+df_sim1['ib'] = 4
+df_sim1p2 = pd.read_csv(sim_dir + 'cmos_tl270u_vdd1p2V_ib4mA_20200527_tt_90C.vcsv', **opt_vcsv)
+df_sim1p2['vdd'] = 1.2
+df_sim1p2['ib'] = 4
+df_sim1p4 = pd.read_csv(sim_dir + 'cmos_tl270u_vdd1p4V_ib4mA_20200527_tt_90C.vcsv', **opt_vcsv)
+df_sim1p4['vdd'] = 1.4
+df_sim1p4['ib'] = 4
+df_sim1p6 = pd.read_csv(sim_dir + 'cmos_tl270u_vdd1p6V_ib6mA_20200527_tt_90C.vcsv', **opt_vcsv)
+df_sim1p6['vdd'] = 1.6
+df_sim1p6['ib'] = 6
+
+# Create one dataframe for easier use
+df_sim = pd.concat([df_sim1, df_sim1p2, df_sim1p4, df_sim1p6], ignore_index=True)
+df_sim.loc[:,'freq'] *= 1e-9
+df_sim.loc[:,'pout'] += 3
+df_sim["eff"] = np.power(10,(df_sim.pout-df_sim.pdc)/10) * 100
+
+######################################
+# Tuning range
+######################################
+fig_s, ax_s = plt.subplots(**plot_style)
+tr_label = []
+cnt = 0
+for cond in l_cond:
+    mask = pd.DataFrame([df_meas[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfm = df_meas[mask]
+    mask = pd.DataFrame([df_sim[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfs = df_sim[mask]
+    assert dfm.ib.nunique()==1, "Multiple bias current values"
+    
+    ax_s.plot(dfs.vtune, dfs.freq, **style_sim) 
+    ax_s.scatter(dfm.vtune, dfm.freq, **l_style_meas[cnt], label="%.1fV %dmA" % (cond["vdd"],cond["ib"])) 
+    cnt += 1
+
+    fmin, fmax = dfm.freq.min(), dfm.freq.max()
+    favg = (fmin+fmax)/2
+    fper = (fmax-favg)/favg*100
+    tr_label.append(r"$\SI{%.1f}{\GHz}\pm\SI{%.1f}{\percent}$" % (favg,fper))
+
+ax_s.plot([],[],**style_sim, label="sim") # to add label
+ax_s.set_xlim(-0.5, 1.5)
+ax_s.set_ylim(48.5, 59)
+ax_s.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(2))
+# ax_s.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(0.5))
+ax_s.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.5))
+# ax_s.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(0.1))
+ax_s.set_xlabel(r"Tuning voltage $\left[\si{\volt}\right]$", labelpad=0)
+ax_s.set_ylabel(r"Frequency $\left [ \si{\GHz} \right ]$", labelpad=1)
+# ax_s.grid()
+
+handles, labels = ax_s.get_legend_handles_labels()
+# # checking order of the labels
+# print(handles)
+# print(labels)
+handles = [*handles[1:], handles[0]]
+labels  = [*labels[1:],  labels[0]]
+leg = ax_s.legend(handles=handles, labels=labels, **legend_style) 
+ax_s.legend(handles=handles[:len(handles)-1], labels=tr_label, **legend_style2, bbox_to_anchor=(0.62,-0.02,0.4,0.2), title="Tuning range")
+ax_s.add_artist(leg)
+ax_s.annotate(**vdd_ib_annot)
+for ext in ["png","pgf"]:
+    # fig_s.savefig(meas_dir + suffix + "_tune." + ext, bbox_inches='tight', pad_inches = 0)
+    fig_s.savefig(meas_dir + suffix + "_tune." + ext, bbox_inches='tight', pad_inches = 0)
+
+######################################
+# Output power
+######################################
+# The differential output power is plotted
+fig_s,ax_s = plt.subplots(**plot_style)
+cnt = 0
+
+pout_label = []
+cond = {"vdd":1, "ib":4}
+for cond in l_cond:
+    mask = pd.DataFrame([df_meas[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfm = df_meas[mask]
+    mask = pd.DataFrame([df_sim[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfs = df_sim[mask]
+    assert dfm.ib.nunique()==1, "Multiple bias current values"
+
+    ax_s.plot(dfs.freq, dfs.pout, **style_sim) 
+    ax_s.scatter(dfm.freq, dfm.pout, **l_style_meas[cnt], label="%.1fV %dmA" % (cond["vdd"],cond["ib"])) 
+    cnt += 1
+
+    p_avg = 10*np.log10(sum(10**(dfm.pout/10))/float(len(dfm.pout))) 
+    pout_label.append("%.1f-%.1f; %.1fdBm" % (dfm.pout.min(), dfm.pout.max(), p_avg)   )
+
+ax_s.plot([],[],**style_sim, label="sim") # to add label
+ax_s.set_xlabel(**xlabel)
+ax_s.set_ylabel("Pout [dBm]")
+ax_s.set_ylabel(r"P\textsubscript{out} $\left[ \si{\dBm} \right ]$")
+ax_s.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(2))
+ax_s.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
+ax_s.set_xlim(50,59)
+ax_s.set_ylim(-1.99,7)
+# ax_s.grid()
+
+handles, labels = ax_s.get_legend_handles_labels()
+# # checking order of the labels
+# print(handles)
+# print(labels)
+handles = [*handles[1:], handles[0]]
+labels  = [*labels[1:], labels[0]]
+leg = ax_s.legend(handles=handles, labels=labels, **legend_style) 
+ax_s.legend(handles=handles[:len(handles)-1], labels=pout_label, **legend_style2_ncol2, bbox_to_anchor=(0.05,-0.02,0.9,0.2), title="P\\textsubscript{out}: min-max; avg")
+ax_s.add_artist(leg)
+ax_s.annotate(**vdd_ib_annot)
+for ext in ["png","pgf"]:
+    fig_s.savefig(meas_dir + suffix + "_pout." + ext) #, bbox_inches='tight', pad_inches = 0)
+
+######################################
+# Power consumption
+######################################
+# The differential output power is plotted
+fig_s,ax_s = plt.subplots(**plot_style)
+fig_s2,ax_s2 = plt.subplots(**plot_style)
+
+pdc_label = []
+pdc_label2 = []
+cnt = 0
+for cond in l_cond:
+    mask = pd.DataFrame([df_meas[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfm = df_meas[mask]
+    mask = pd.DataFrame([df_sim[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfs = df_sim[mask]
+    assert dfm.ib.nunique()==1, "Multiple bias current values"
+
+    ax_s.plot(dfs.freq, dfs.pdc, **style_sim)
+    ax_s2.plot(dfs.freq, np.power(10,dfs.pdc/10), **style_sim)
+    meas_pdc_mW = dfm.vdd * dfm.idd
+    meas_pdc = 10*np.log10(meas_pdc_mW)
+    ax_s.scatter(dfm.freq, meas_pdc, **l_style_meas[cnt], label=r"\SI{%.1f}{\volt} \SI{%d}{\mA}" % (cond["vdd"],cond["ib"])) 
+    ax_s2.scatter(dfm.freq, meas_pdc_mW, **l_style_meas[cnt], label=r"\SI{%.1f}{\volt} \SI{%d}{\mA}" % (cond["vdd"],cond["ib"])) 
+    cnt += 1
+
+    pdc_label.append("\SIrange{%.1f}{%.1f}{}; \SI{%.1f}{\dBm}" % (meas_pdc.min(), meas_pdc.max(), meas_pdc.mean())   )
+    pdc_label2.append("\SIrange{%.1f}{%.1f}{}; \SI{%.1f}{\mW}" % (meas_pdc_mW.min(), meas_pdc_mW.max(), meas_pdc_mW.mean())   )
+
+
+ax_s.plot([],[],**style_sim, label="sim") # to add label
+ax_s2.plot([],[],**style_sim, label="sim") # to add label
+ax_s.set_xlabel(**xlabel)
+ax_s.set_ylabel(r"P\textsubscript{DC} $\left [ \si{\dBm} \right ]$")
+ax_s.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(2.5))
+ax_s.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
+ax_s.set_xlim(50,59)
+ax_s.set_ylim(2.6,15)
+# ax_s.grid()
+ax_s2.set_xlabel(**xlabel)
+ax_s2.set_ylabel(r"P\textsubscript{DC} $\left [ \si{\mW} \right ]$")
+ax_s2.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(5))
+ax_s2.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
+ax_s2.set_xlim(50,59)
+ax_s2.set_ylim(5,40)
+# ax_s2.grid()
+
+handles, labels = ax_s.get_legend_handles_labels()
+# # checking order of the labels
+# print(handles)
+# print(labels)
+hand = [*handles[1:], handles[0]]
+lab = [*labels[1:], labels[0]]
+leg = ax_s.legend(handles=hand, labels=lab, **legend_style) 
+ax_s.legend(handles=handles[1:], labels=pdc_label, **legend_style2_ncol2, bbox_to_anchor=(0.05,-0.02,0.9,0.2), title="P\\textsubscript{DC}: min-max; avg")
+ax_s.add_artist(leg)
+leg = ax_s2.legend(handles=hand, labels=lab, **legend_style) 
+ax_s2.legend(handles=handles[1:], labels=pdc_label2, **legend_style2_ncol2, bbox_to_anchor=(0.05,0.65,0.9,0.2), title="P\\textsubscript{DC}: min-max; avg")
+ax_s2.add_artist(leg)
+ax_s.annotate(**vdd_ib_annot)
+ax_s2.annotate(**vdd_ib_annot)
+for ext in ["png","pgf"]:
+    fig_s.savefig(meas_dir + suffix + "_pdc." + ext) #, bbox_inches='tight', pad_inches = 0)
+    fig_s2.savefig(meas_dir + suffix + "_pdc_mW." + ext) #, bbox_inches='tight', pad_inches = 0)
+
+######################################
+# DC to RF efficiency
+######################################
+# l_eff = [eff(p_out[i], VDD, IDD[i], in_dBm=True) for i in range(len(p_out))]
+fig_s,ax_s = plt.subplots(**plot_style)
+eff_label = []
+cnt = 0
+
+for cond in l_cond:
+    mask = pd.DataFrame([df_meas[key] == val for key, val in cond.items()]).T.all(axis=1)
+    dfm = df_meas[mask]
+    # mask = pd.DataFrame([df_sim[key] == val for key, val in cond.items()]).T.all(axis=1)
+    # dfs = df_sim[mask]
+    assert dfm.ib.nunique()==1, "Multiple bias current values"
+    # ax_s.plot(dfs.freq, dfs.eff, **style_sim) 
+    ax_s.scatter(dfm.freq, dfm.eff, **l_style_meas[cnt], label="%.1fV %dmA" % (cond["vdd"],cond["ib"])) 
+    cnt += 1
+
+    eff_min, eff_max, eff_mean = dfm.eff.min(), dfm.eff.max(), dfm.eff.mean()
+    eff_label.append("%.f-%.f%%; %.f%%" % (eff_min, eff_max, eff_mean))
+
+# ax_s.plot([],[],**style_sim, label="sim") # to add label
+# ax_s2.plot([],[],**style_sim, label="sim") # to add label
+ax_s.set_xlabel(**xlabel)
+ax_s.set_ylabel(r"$\eta \left [ \si{\percent} \right ]$")
+ax_s.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(2.5))
+ax_s.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
+ax_s.set_xlim(50,59)
+# ax_s.set_ylim(14,25)
+# ax_s.grid()
+
+handles, labels = ax_s.get_legend_handles_labels()
+# # checking order of the labels
+# print(handles)
+# print(labels)
+label_order = [1, 2, 0]
+# handles = [*handles[1:], handles[0]]
+# labels  = [*labels[1:],  labels[0]]
+# leg = ax_s.legend(handles=handles, labels=labels, **legend_style_ncol2) 
+leg = ax_s.legend(**legend_style_ncol2) # no simulation
+# ax_s.legend(handles=handles[:len(handles)-1], labels=eff_label, **legend_style2_ncol2, bbox_to_anchor=(0.15,-0.02,0.7,0.2), title="$\eta$: min-max; avg")
+ax_s.legend(labels=eff_label, **legend_style2_ncol2, bbox_to_anchor=(0,0.65,0.65,0.2), title="$\eta$: min-max; avg")
+ax_s.add_artist(leg)
+ax_s.annotate(**vdd_ib_annot)
+# if suffix == "cmos_tl270u":
+#   ax_s.text(max(f_tune)*0.97,30,"%.1f%% - %.1f%%\nAvg: %.1f%%" %( min(l_eff),max(l_eff),sum(l_eff)/float(len(l_eff))),ha="center")
+for ext in ["png","pgf"]:
+    fig_s.savefig(meas_dir + suffix + "_eff." + ext) #, bbox_inches='tight', pad_inches = 0)
